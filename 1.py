f845859e8c44dd5c24dfcc5e7ae248dd830ae0fe
slakov <slakov@gmail.com>
2018-09-02 21:38:50 +0200

    tmp

--- 1.py
+++ 1.py
@@ -1,21 +1,38 @@
 import pandas as pd
 import json
 import random
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+# Sanity check that python3
 print ("Magic starts");
 
-with open("db.json") as datafile:
+# Read data from JSON
+with open("db-3.json") as datafile:
     data = json.load(datafile)
     d = pd.DataFrame(data)
     df = d.set_index("name", drop = False)
 
+# Do pair-wise matching
 nOfEntries = len(df.index)
 last = nOfEntries - 1
+
+c1 = []
+c2 = []
+pScore = []
 
 for i in range(last):
     for j in range((i+1),(last+1)):
         firstCompany = df.iloc[i]
         secondCompany = df.iloc[j]
-        print(firstCompany["name"] + "\t\t" +
-        secondCompany["name"] + "\t\t" + str(random.randint(50,99)) +"%")
+        c1.append(firstCompany["name"])
+        c2.append(secondCompany["name"])
+        #print(firstCompany["name"] + "\t\t" +
+        #secondCompany["name"] + "\t\t" + str(random.randint(50,99)) +"%")
+
+        vect = TfidfVectorizer(min_df=1)
+        tfidf = vect.fit_transform([firstCompany["added"], secondCompany["added"]])
+        res = (tfidf * tfidf.T).A
+        print(res)
+
+#print(c1)
